2be183fe248edcd477368b237d57a062ff177194
pramod-hamal <pramod.th244@gmail.com>
2022-11-01 12:40:58 +0545

    https in api docs

--- config/db.js
+++ config/db.js
@@ -1,20 +1,17 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 // "mongodb+srv://nsph:nsph%40123@nsph.5a73bwd.mongodb.net/?retryWrites=true&w=majority"
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.DB_URL,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      }
-    );
+    await mongoose.connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log('MongoDB is Connected...');
+    console.log("MongoDB is Connected...");
   } catch (err) {
-    console.log("error is", err)
+    console.log("error is", err);
     console.error(err.message);
     process.exit(1);
   }
