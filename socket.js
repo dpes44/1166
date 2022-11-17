f78a696ddb40f8980f2ad479bf562ec4feea14dd
Pandora Pradip <pradip@pandoragco.com>
2022-11-17 16:25:18 +0545

    comment added

--- socket.js
+++ socket.js
@@ -14,6 +14,7 @@ module.exports = function (server) {
     io.use((socket, next) => {
         const token = socket.handshake.headers['x-access-token'];
         console.log(token);
+        // verify user 
         if (true) {
             next();
         } else {
@@ -23,6 +24,9 @@ module.exports = function (server) {
 
     io.on("connection", data => {
         console.log("User connected with", data.id);
+
+        // updated user with new socket id 
+        
         data.on("disconnect", () => {
             console.log("User disconnected");
         });
