6505f70ea1394e2e895065e530e223c45cae1a52
Pandora Pradip <pradip@pandoragco.com>
2022-11-22 12:54:43 +0545

    chat list added

--- models/ChatList.js
+++ models/ChatList.js
@@ -13,11 +13,16 @@ const ChatSchema = new mongoose.Schema(
       type: mongoose.Types.ObjectId,
       ref: "user",
       required: [true, "receiver is required"],
-    }
+    },
+
   },
   {
     timestamps: true,
-  }
+    toJSON: {
+      getters: true,
+      setters: true,
+    }
+  },
 );
 
 // add function 
@@ -38,5 +43,27 @@ ChatSchema.statics.findOrCreate = function (userOne, userTwo) {
     });
   });
 };
+
+
+ChatSchema.statics.getChatList = function (_id) {
+  const Chat = this;
+  return Chat.find({
+    $or: [
+      { userOne: _id },
+      { userTwo: _id },
+    ],
+  }).populate(["userOne", "userTwo"]).sort({ createdAt: "desc" }).then((chats) => {
+    return chats.map((chat) => {
+      if (chat.userOne._id.toString() === _id.toString()) {
+        return chat.userTwo;
+      }
+      return chat.userOne;
+    });
+  });
+};
+
+// ChatSchema.set('toJson', { virtual: true })
+
+
 
 module.exports = ChatList = mongoose.model("chatList", ChatSchema);
--- routes/api/message.js
+++ routes/api/message.js
@@ -1,7 +1,18 @@
 const { returnResponse } = require("../../helper/response.helper");
 
 const router = require("express").Router()
-router.get("/:userId", async function(req, res, next) {
+
+router.get("/chat-list", async function (req, res, next) {
+    try {
+        // console.log("req.user._id", req.user)
+        const chatLists = await NSPH_DB.ChatList.getChatList(req.user._id);
+        return returnResponse(res, chatLists);
+    } catch (error) {
+        console.log("error is", error)
+        return next(error)
+    }
+})
+router.get("/:userId", async function (req, res, next) {
     try {
         const messages = await NSPH_DB.Messages.find({
             $or: [
@@ -15,4 +26,6 @@ router.get("/:userId", async function(req, res, next) {
         return next(error)
     }
 })
+
+
 module.exports = router;
